f2712014d8433336a989c8cafcf033a109f72170
Adrian Lucrèce Céleste <adrianlucrececeleste@airmail.cc>
2018-07-04 13:52:23 -0400

    [script] Add release variable to make script more flexible

--- array.sh
+++ array.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+release="52.9.0"
 
 for i in ar ast be bg bn-BD br ca cs cy da de dsb el en-GB en-US es-AR es-ES et eu fr fi fy-NL ga-IE gd gl he hr hsb hu hy-AM id is it ja kab ko lt nb-NO nn-NO pa-IN pl pt-BR pt-PT rm ro ru si sk sl sq sr sv-SE ta-LK tr uk vi zh-CN zh-TW ; do
-    wget -q https://download-origin.cdn.mozilla.net/pub/thunderbird/releases/52.9.0/linux-x86_64/xpi/${i}.xpi
+    wget -q https://download-origin.cdn.mozilla.net/pub/thunderbird/releases/${release}/linux-x86_64/xpi/${i}.xpi
     filesize=$(du -b ${i}.xpi | sed -e 's/\t\w*.*//g')
     checksum=$(sha256sum ${i}.xpi | sed -e 's/\ \w*.*//g')
     echo "{
@@ -10,7 +11,7 @@ for i in ar ast be bg bn-BD br ca cs cy da de dsb el en-GB en-US es-AR es-ES et 
             \"only-arches\": [
                 \"x86_64\"
             ],
-            \"url\": \"https://download-origin.cdn.mozilla.net/pub/thunderbird/releases/52.9.0/linux-x86_64/xpi/et.xpi\",
+            \"url\": \"https://download-origin.cdn.mozilla.net/pub/thunderbird/releases/${release}/linux-x86_64/xpi/et.xpi\",
             \"sha256\": \"${checksum}\",
             \"size\": \"${filesize}\"
         },"
