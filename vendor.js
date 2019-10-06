035873bdba2d8746b23b7eb2458dc774f1fcd8fb
Erick555 <37542552+Erick555@users.noreply.github.com>
2019-10-06 14:07:53 +0200

    Add vendor.js and remove calendar
    This should make changing language easier.
    
    Also other minor fixes and README improvements.

---
+++ vendor.js
@@ -0,0 +1,13 @@
+// Disable autopdates
+pref("app.update.enabled", false);
+pref("app.update.autoInstallEnabled", false);
+
+// Use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Use the system locale
+pref("intl.locale.requested", "");
+
+// Disable default mailer checking.
+pref("mail.shell.checkDefaultClient", false);
+pref("mail.shell.checkDefaultMail", false);
