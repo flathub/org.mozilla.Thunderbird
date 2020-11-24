cfafcb1736f5325424e94d03a1f395d083cb8e83
Erick555 <37542552+Erick555@users.noreply.github.com>
2020-11-24 21:55:10 +0100

    Minor lint fixes for update script

--- update-thunderbird-version.sh
+++ update-thunderbird-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-if (( $# < 2 )); then
+if (($# < 2)); then
   echo "Usage: $0 VERSION RELEASE_DATE"
   echo ""
   echo "Example: $0 78.4.3 2020-11-11"
@@ -17,13 +17,13 @@ SOURCES_FILE="$PACKAGE-sources.json"
 APPDATA_FILE="org.mozilla.Thunderbird.appdata.xml"
 
 # check provided release date
-if ! [[ $RELEASE_DATE =~ ^20[0-9]{2}-(0[0-9]|1[0-2])-([0-2][0-9]|3[01])$ ]]; then
+if ! [[ "$RELEASE_DATE" =~ ^20[0-9]{2}-(0[0-9]|1[0-2])-([0-2][0-9]|3[01])$ ]]; then
   echo >&2 "Invalid release date '$RELEASE_DATE'. Please provide the date in the format YYYY-MM-DD, e.g. 2021-01-28."
   exit 1
 fi
 
 # write new sources file
-echo '[' > "$SOURCES_FILE"
+echo '[' >"$SOURCES_FILE"
 
 # read files from SHA256SUMS file
 while read -r line; do
@@ -45,7 +45,7 @@ while read -r line; do
     locale="${path##*/}"
     locale="${locale%.*}"
 
-    cat >> "$SOURCES_FILE" <<EOT
+    cat >>"$SOURCES_FILE" <<EOT
     {
         "type": "file",
         "url": "$BASE_URL/$path",
@@ -58,10 +58,10 @@ EOT
 done < <(curl -Ss "$BASE_URL/SHA256SUMS" | grep "^\S\+  \(source\|$PLATFORM/xpi\)/")
 
 # add source archive entry to sources file
-echo -e "$source_archive\n]" >> "$SOURCES_FILE"
+echo -e "$source_archive\n]" >>"$SOURCES_FILE"
 
 # update releases in appdata file
-sed -ri 's@^(\s+<release version=")[^"]+(" date=")[^"]+(" />)$@'"\1${VERSION}\2${RELEASE_DATE}\3@" "$APPDATA_FILE"
+sed -ri 's@^(\s+<release version=")[^"]+(" date=")[^"]+(" />)$@'"\1$VERSION\2$RELEASE_DATE\3@" "$APPDATA_FILE"
 
 cat <<EOT
 The files were successfully updated to version $VERSION.
